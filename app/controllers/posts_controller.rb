4917370100ea056cd5061043794b53f7952fd6c2
uch1104 <foajoaquin@gmail.com>
2020-10-22 18:18:40 +0900

    メモ投稿機能のフロントエンド実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,7 +5,7 @@ class PostsController < ApplicationController
   end
   def create
     post = Post.create(content: params[:content])
-    render json:{ post; post }
+    render json:{ post: post }
   end
   def checked
     post = Post.find(params[:id])
